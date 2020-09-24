1ac4cd1f572fbb29b3ed1485394d8f86849ed157
kleksy <krzysztof.leksy@gmail.com>
2020-09-24 21:59:59 +0200

    Initial commit

---
+++ src/main/java/pl/leksy/krzysztof/chat/server/ChatServerApplication.java
@@ -0,0 +1,13 @@
+package pl.leksy.krzysztof.chat.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatServerApplication.class, args);
+	}
+
+}
